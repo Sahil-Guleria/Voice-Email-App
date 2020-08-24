d79f702dc2624faaa44e1d29b0b07684756ccf1e
Sahil Guleria <68143032+Sahil-Guleria@users.noreply.github.com>
2020-08-24 20:29:42 +0530

    Update Utils.java

--- app/src/main/java/com/example/voiceemailapp/Utils.java
+++ app/src/main/java/com/example/voiceemailapp/Utils.java
@@ -1,6 +1,6 @@
 package com.example.voiceemailapp;
 
 public class Utils {
-    public static final String EMAIL = "guleriasahil1999@gmail.com";
-    public static final String PASSWORD = "Sahil!@#123";
+    public static final String EMAIL = "YOUR_EMAIL_ID";
+    public static final String PASSWORD = "PASSWORD";
 }
